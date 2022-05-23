df85bda2085f4ee337bb5aa6a71f2c25e8083750
Chrwa <Chrward23@gmail.com>
2022-05-22 20:54:54 -0500

    Password Generator complete

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Magic-Wizard-Password
 Module 3 Challenge
+In this challenge we were tasked with creating a password generator.
+I was given starter code that had html and css already.
+Javascript was also included but other functions needed to be added to make it work.
+I added the generatePassword function to the script.
+Now the password generator asks the user prompts about they password they want generated.
+The password generator function runs over a for loop fpr the length of the array that the user selected.
+Ive tested the password generator by changing variables and trying to add paparmeters outside of the acceptance criteria.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,77 @@
 // Assignment code here
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] 
-var specialChar = ['!', '@', '#', '$', '%', '&', '*'] 
-var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialChar = ['!', '@', '#', '$', '%', '&', '*'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var passwordLength = 8;
+var passwordPool = [];
+specialChar[2]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword () {
-console.log('Hey you clicked the button')
-window.prompt("Choose password length. Atleast 8 characters but no more than 128");
-window.prompt("Should password include Upper Case Letters? YES or NO");
-window.prompt("Should password include Lower Case Letters? YES or NO");
-window.prompt("Should password include Numbers? YES or NO"); 
-window.prompt("Should password include Special Characters? YES or NO");
-
-return "Generated password will go here"
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);// Write password to the #password input
+
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
+
+
+
+
+
+function generatePassword() {
+
+    passwordLength = parseInt(window.prompt("Choose password length. Atleast 8 characters but no more than 128"));
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+        window.alert('Please choose something between 8 and 128')
+        return null
+    }
+
+    var promptUpper = window.confirm("Should password include Upper Case Letters? Click Ok");
+
+    var promptLower = window.confirm("Should password include Lower Case Letters? Click Ok");
+
+    var promptNum = window.confirm("Should password include Numbers? Click Ok");
+
+    var promptSpecial = window.confirm("Should password include Special Characters? Click Ok");
+
+    if (promptUpper) {
+        passwordPool = passwordPool.concat(upperCase)
+    }
+    if (promptLower) {
+        passwordPool = passwordPool.concat(lowerCase)
+    }
+    if (promptNum) {
+        passwordPool = passwordPool.concat(numbers)
+    }
+    if (promptSpecial) {
+        passwordPool = passwordPool.concat(specialChar)
+    }
+    console.log(passwordPool)
+    var  newPass = []
+    //Generate password
+    for (var i = 0; i < passwordLength; i++) {
+        newPass.push(passwordPool[Math.floor(Math.random() * passwordPool.length)])
+        console.log(newPass)
+    }
+    console.log(newPass)
+    newPass = newPass.join('')
+    console.log(newPass)
+    return newPass
+}
+
+
+
+
+
+
 
 
 
